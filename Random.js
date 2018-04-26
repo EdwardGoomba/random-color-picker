64583377514bebc9e68a853f121c94fabe34d4ef
EdwardGoomba <ask@edwardvdesign.com>
2018-04-25 22:54:17 -0400

    Add button and change color on click

--- Random.js
+++ Random.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from './Button';
 
 class Random extends React.Component {
     constructor(props) {
         super(props);
         this.state = { color: [17, 44, 124] };
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
@@ -37,12 +39,21 @@ class Random extends React.Component {
         return random;
     }
 
+    handleClick() {
+        this.setState({
+            color: this.chooseColor()
+        });
+    }
+
     render() {
         return (
             <div>
                 <h1 className={this.isLight() ? 'white' : 'black'}>
                     'Your color is {this.formatColor(this.state.color)}.'
-        </h1>
+                </h1>
+                <Button
+                    light={this.isLight()}
+                    onClick={this.handleClick} />
             </div>
         );
     }
